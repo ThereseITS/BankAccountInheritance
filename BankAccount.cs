50e5fb8c890665423447e26193f7a32641bd57a9
Therese Hume <Therese.Hume@atu.ie>
2025-04-25 17:24:32 +0100

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountInheritance
+{
+    internal class BankAccount
+    {
+        string _accountNumber;
+        static int accountNumberGenerator =0;
+        public string AccountNumber {
+            get { return _accountNumber; }        
+            private set { _accountNumber = value; } 
+        }
+        public decimal Balance { get; protected set; }
+
+        /// <summary>
+        /// Basic Constructor
+        /// </summary>
+        public BankAccount() 
+        { 
+            Balance = 0;
+            accountNumberGenerator++;
+            _accountNumber = accountNumberGenerator.ToString();          
+        }
+        /// <summary>
+        /// Constructor to set the initial balance
+        /// </summary>
+        /// <param name="balance"></param>
+        public BankAccount(decimal balance)
+        {
+            this.Balance = balance;
+            accountNumberGenerator++;
+            _accountNumber = accountNumberGenerator.ToString();
+        }
+        /// <summary>
+        /// Constructor for pre-existing account
+        /// </summary>
+        /// <param name="accountNumber"></param>
+        /// <param name="balance"></param>
+        public BankAccount(string  accountNumber, decimal balance)
+        { 
+            this.Balance = balance;
+            this._accountNumber = accountNumber;
+        }
+
+        /// <summary>
+        /// Lodge money. Returns new balance.
+        /// </summary>
+        /// <param name="amount"> amount of money to lodge</param>
+        public decimal Lodge(decimal amount)
+        {
+            Balance += amount;
+            return Balance;
+        }
+/// <summary>
+/// If there is enough money,this method subtracts the amount from the balance, returning the amount.
+/// If there is not enough money, this method sets the balance to zero, returning the available amount.
+///= </summary>
+/// <param name="amount"></param>
+/// <returns>amount withdrawn</returns>
+        public virtual decimal Withdraw(decimal amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+
+            }
+            else
+            {
+                amount = Balance;
+                Balance = 0;
+            }
+            return amount;
+        }
+        public override string ToString()
+        {
+            return $"Account Number : {_accountNumber} Balance: {Balance}";
+        }
+    }
+    /// <summary>
+    /// Child clsas - current account
+    /// </summary>
+    class CurrentAccount:BankAccount
+    {
+        public decimal OverdraftLimit { get; private set; }
+
+        public CurrentAccount() : base()
+        {
+            OverdraftLimit = 0;
+        }
+        public CurrentAccount(decimal balance, decimal overdraftLimit):base(balance)
+        {
+            this.OverdraftLimit = overdraftLimit;
+        }
+        public CurrentAccount(string accountNumber, decimal balance, decimal overdraftLimit) : base(accountNumber, balance)
+        {
+            this.OverdraftLimit = overdraftLimit;
+        }
+
+        public void SetNewOverdraft(decimal amount)
+        {
+            OverdraftLimit = amount;
+        }
+        public override decimal Withdraw(decimal amount)
+        {
+            if ((Balance+OverdraftLimit) >= amount)
+            {
+                Balance -= amount;
+
+            }
+            else
+            {
+                amount = Balance+OverdraftLimit;
+                Balance = OverdraftLimit*(-1);
+               
+            }
+            return amount;
+        }
+        public override string ToString()
+        {
+            return $" Current {base.ToString()} Overdraft Limit: {OverdraftLimit.ToString()}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace BankAccountInheritance
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            BankAccount[] bcs = new BankAccount[3];
+
+            bcs[0] = new BankAccount(200m);         
+            bcs[1] = new CurrentAccount(300m,100m);
+            bcs[2] = new CurrentAccount(0,500m);
+
+     // Try and withdraw 500 from each account       
+
+            foreach (BankAccount bc in bcs)
+            {
+                bc.ToString();
+                Console.WriteLine(bc.ToString());
+                Console.WriteLine($"You have withdrawn {bc.Withdraw(500)} from {bc.AccountNumber} Balance now {bc.Balance}");
+                bc.ToString();
+                Console.WriteLine("------------");
+            }
+
+            Console.WriteLine(" Withdrawing money from empty accounts");
+
+            foreach (BankAccount bc in bcs)
+            {
+                bc.ToString();
+                Console.WriteLine($"You have withdrawn {bc.Withdraw(500)} from {bc.AccountNumber} Balance now {bc.Balance}");
+                bc.ToString();
+                Console.WriteLine("------------");
+            }
+
+
+            Console.WriteLine(" Lodging money ");
+
+            foreach (BankAccount bc in bcs)
+            {
+                bc.ToString();
+                Console.WriteLine($"You have lodged 300 to {bc.AccountNumber} Balance now: {bc.Lodge(300)}");
+              
+                Console.WriteLine("------------");
+            }
+            //Note that to use the child class method, the object needs to be explicitly declared as a member of the child class
+
+            CurrentAccount c = new CurrentAccount(500m, 500m);
+            Console.WriteLine("------------");
+
+            Console.WriteLine("Re-Setting the overdraft limit");
+
+            Console.WriteLine(c.ToString());
+            c.SetNewOverdraft(100);
+            Console.WriteLine(c.ToString());
+        }
+    }
+}
